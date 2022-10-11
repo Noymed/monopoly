a70cd6c721e0ba58853a9f4af5cdcc0579235b56
Noymed <noymedina@gmail.com>
2022-10-11 21:45:59 +0300

    add landing on go to jail

--- main.py
+++ main.py
@@ -26,6 +26,12 @@ def main():
     jail = False  # flag to know if we are in jail or just visiting jail
     for i in range(10):
         position, double = movement(position)
+        # landing on go to jail
+        if position == 30:
+            print("landed on go to jail")
+            double = False  # so we won't have another throw
+            jail = True
+            position = 10
         while double:
             double_streak += 1
             print("streak: ", double_streak)  # checking the double streak counter
